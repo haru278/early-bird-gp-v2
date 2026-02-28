b4ba95ae5675a23c88f8dbfcc519bd5b56aa812b
三宅治輝 <miyakeharuki@miyakeosamuakiranoMacBook-Air.local>
2026-02-28 18:58:59 +0900

    Fix: Enable Metro symlinks for pnpm error

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,10 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// pnpmのシンボリックリンクとパッケージエクスポートを有効化（エラー解決の鍵）
+config.resolver.unstable_enableSymlinks = true;
+config.resolver.unstable_enablePackageExports = true;
+
 module.exports = withNativeWind(config, {
   input: "./global.css",
   // Force write CSS to file system instead of virtual modules
